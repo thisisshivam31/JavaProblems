2a5cde3ef25aed1c649fbc016e58c222ebf91fc9
SHIVAM SINGH <49497648+thisisshivam31@users.noreply.github.com>
2022-09-27 16:25:29 +0530

    Create 4. Write a Java program to print the result of the following operations.java

---
+++ 4. Write a Java program to print the result of the following operations.java
@@ -0,0 +1,12 @@
+public class Main
+{
+  public static void main (String[]args)
+  {
+    int a = 50;
+    int b = 3;
+      System.out.println (-5 + 8 * 6);
+      System.out.println ((55+9) % 9);
+      System.out.println (20 + -3*5 / 8);
+      System.out.println (5 + 15 / 3 * 2 - 8 % 3);
+  }
+}
