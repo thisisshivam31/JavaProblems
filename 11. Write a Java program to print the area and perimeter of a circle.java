3180706f4b1db05dd7d18340761c9893c51402ef
SHIVAM SINGH <49497648+thisisshivam31@users.noreply.github.com>
2022-09-27 16:16:26 +0530

    Create 11. Write a Java program to print the area and perimeter of a circle.java

---
+++ 11. Write a Java program to print the area and perimeter of a circle.java
@@ -0,0 +1,13 @@
+import java.lang.Math.*;
+
+public class Main
+{
+	public static void main(String[] args) {
+	    double r = 7.5;
+	    double p = 2*Math.PI*r;
+	    double a = Math.PI*r*r;
+	    
+		System.out.println(p);
+		System.out.println(a);
+	}
+}
