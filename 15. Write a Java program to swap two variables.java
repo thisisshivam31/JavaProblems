fe201167314c75254bc0d220f568e4ef652c843a
SHIVAM SINGH <49497648+thisisshivam31@users.noreply.github.com>
2022-10-02 13:08:45 +0530

    Create 15. Write a Java program to swap two variables.java

---
+++ 15. Write a Java program to swap two variables.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+public class Main
+{
+	public static void main(String[] args) {
+	  
+        Scanner sc = new Scanner(System.in);
+        int x = sc.nextInt();
+        int y = sc.nextInt();
+        
+        x=x+y;
+        y=x-y;
+        x=x-y;
+ 
+        System.out.println(x);
+        System.out.println(y);
+   
+	}
+}
