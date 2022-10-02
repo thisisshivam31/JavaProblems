cda0d9cc5de3792aa4f0e7e0d47d9b0d83555569
SHIVAM SINGH <49497648+thisisshivam31@users.noreply.github.com>
2022-10-02 12:58:50 +0530

    Create 13. Write a Java program to print the area and perimeter of a rectangle.java

---
+++ 13. Write a Java program to print the area and perimeter of a rectangle.java
@@ -0,0 +1,12 @@
+public class Main
+{
+	public static void main(String[] args) {
+	  
+	    final double w = 5.6;
+	    final double h = 8.5;
+	    
+	    
+		System.out.println("Area" + w*h);
+		System.out.println("Perimeter" + 2*(w+h));
+	}
+}
