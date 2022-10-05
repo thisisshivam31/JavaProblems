8f1b44ea62098108fb243c8ded2d04dda523f8cc
SHIVAM SINGH <49497648+thisisshivam31@users.noreply.github.com>
2022-10-05 21:14:37 +0530

    Create 31. Write a Java program to check whether Java is installed on your computer.java

---
+++ 31. Write a Java program to check whether Java is installed on your computer.java
@@ -0,0 +1,11 @@
+public class Main
+{
+	public static void main(String[] args) {
+		System.out.println(System.getProperty("java.version"));
+		System.out.println(System.getProperty("java.Runtime.version"));
+		System.out.println(System.getProperty("java.home"));
+		System.out.println(System.getProperty("java.vendor"));
+		System.out.println(System.getProperty("java.vendor.url"));
+		System.out.println(System.getProperty("java.class.path"));
+	}
+}
